ae65652d8a2b59c9618b00974b8ee17995ec852f
filipwroblewski <filip.wroblewski.row@gmail.com>
2024-01-14 15:28:31 +0100

    create Task model

---
+++ task_manager/task_manager/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
